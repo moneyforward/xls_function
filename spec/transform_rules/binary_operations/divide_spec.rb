5eaa71cfb59bce6e842c0c1c10e9d7cdd3820349
koyo-miyamura <nexus9071@gmail.com>
2023-01-13 18:51:00 +0900

    Add divide_spec to test zero division

--- spec/transform_rules/binary_operations/divide_spec.rb
+++ spec/transform_rules/binary_operations/divide_spec.rb
@@ -1,8 +1,21 @@
 RSpec.describe XlsFunction::Transform do
   subject { described_class.new.apply(obj, context).evaluate }
 
-  let(:obj) { { left: { number: '9' }, operator: '/', right: { number: '3' } } }
-  let(:context) { nil }
+  context 'when right is not zero' do
+    let(:obj) { { left: { number: '9' }, operator: '/', right: { number: '3' } } }
+    let(:context) { nil }
 
-  it { is_expected.to eq(3) }
+    it { is_expected.to eq(3) }
+  end
+
+  context 'when right is zero' do
+    let(:obj) { { left: { number: '9' }, operator: '/', right: { number: '0' } } }
+    let(:context) { nil }
+
+    it do
+      is_expected.to be_error
+        .and be_is_a(XlsFunction::ErrorValue)
+        .and eq("#DIV/0!")
+    end
+  end
 end
