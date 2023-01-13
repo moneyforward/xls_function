ff72d1ede733a9f56c51dfd6332dd4765a89222d
koyo-miyamura <nexus9071@gmail.com>
2023-01-13 18:50:03 +0900

    Add error for dividing by zero

--- lib/xls_function/evaluators/binary_operations/divide.rb
+++ lib/xls_function/evaluators/binary_operations/divide.rb
@@ -5,7 +5,16 @@ module XlsFunction
         operator_as '/'
 
         def eval
+          return ::XlsFunction::ErrorValue.div0!(divide_by_zero) if right.zero?
+
           left / right
+        end
+
+        private
+
+        def divide_by_zero
+          message = error_message(:cannot_divide_by_zero)
+          class_info(message)
         end
       end
     end
