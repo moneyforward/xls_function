b066e5d4e257fb69bca6be69fd93a793b032105d
kashimoto.noriko <>
2024-02-08 17:55:42 +0900

    fix that twice runs methods

--- lib/xls_function/evaluators/functions/switch.rb
+++ lib/xls_function/evaluators/functions/switch.rb
@@ -12,9 +12,12 @@ module XlsFunction
           condition = arg_list.first.evaluate(context)
 
           arg_list[1..-1].each_slice(2) do |expr, value|
-            return value&.evaluate(context) if expr&.evaluate(context) == condition
+            ex_value = expr&.evaluate(context)
+            in_value = value&.evaluate(context)
 
-            return expr&.evaluate(context) if value&.evaluate(context).nil?
+            return in_value if ex_value == condition
+
+            return ex_value if in_value.nil?
           end
 
           XlsFunction::ErrorValue.na(class_info(error_message(:missing_value_for_function)))
